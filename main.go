912dacc16ae7ea56bee5eeb4f35946c701db6ce5
chuokoh <chukwuebukajosh8@gmail.com>
2026-03-16 10:48:27 +0100

    III

--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
+
+type kv struct{
+	key string
+	value int
+}
 
 func main() {
 	content, err := os.ReadFile("text.txt")
@@ -20,8 +26,24 @@ func main() {
 
 	for _, word := range splitWord{
 		wordCount[word] ++
-
 	}
 
-	fmt.Println(wordCount)
+	var sliceOfStruct []kv
+
+	for i, k := range wordCount{
+		sliceOfStruct = append(sliceOfStruct, kv{i, k})
+	}
+
+	sort.Slice(sliceOfStruct, func(i, j int) bool {
+    	return sliceOfStruct[i].value > sliceOfStruct[j].value
+	})
+
+	var finalReslt string
+
+	for _, i := range sliceOfStruct {
+		line := fmt.Sprintf("%s: %d\n", i.key, i.value)
+		finalReslt += line
+	}
+
+	os.WriteFile("result.txt", []byte(finalReslt), 0644)
 }
